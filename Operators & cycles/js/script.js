e033562e34fc88324e432e0da99dcfeec0543cc3
Alexander Vasuluev <alexhateful@gmail.com>
2019-03-28 20:06:34 +0200

    add opertors and cycles exmaples

---
+++ Operators & cycles/js/script.js
@@ -0,0 +1,75 @@
+// var a = b = 3;
+
+// if (a > b) {
+//     console.log( "a > b" );
+// }
+// else if ( a == b  ) {
+//     console.log( "b = a" );
+// }
+// else {
+//     console.log( "b > a" )
+// }
+
+// switch ( prompt("Пароль?") ) {
+//     case 'Чёрный Властелин':
+//         alert("Добро пожаловать!")
+//         // break;
+//     case null:
+//         alert("Вход отменен");
+//         break;
+//     default:
+//         alert('Пароль неверен');
+//         break;
+// }
+
+// if (3 > 5) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// (3 > 5) ? console.log(true) : console.log(false); 
+
+// while ( confirm("Ты голодный?") ) {
+//     alert("Пойди поешь!");
+// }
+
+// var a = 0;
+
+// while ( ++a < 5 ) {
+//     console.log(a);
+// }
+
+// a++ < 5
+// 1) увеличение а на 1
+// 2) проверка условий
+
+// ++a < 5
+// 1) проверка условий
+// 2) увеличение а на 1
+
+// var b = 4;
+
+// do {
+//     console.log(b);
+//     b++;
+// } while ( b < 5 );
+
+// var count = "3";
+
+// if ( typeof(count) == 'number' ) {
+//     console.log('count is string');
+// }
+// else {
+//     count = parseInt(count);
+//     console.log( typeof(count) );
+// }
+
+// var symbol = 'A';
+// var count = parseInt( prompt('сколько?') );
+// var result = "";
+
+// for (let i = 0; i < count; i++) {
+//     console.log( result += symbol );
+// }
