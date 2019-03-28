36ca4f3c51dee7eab83286715290419bdf20999e
Alexander Vasuluev <alexhateful@gmail.com>
2019-03-28 20:27:51 +0200

    add examples

---
+++ Basic js/js/script.js
@@ -0,0 +1,26 @@
+// // Сумма чисел
+// // Создайте страницу, которая предлагает ввести два числа и выводит их сумму.
+// /**
+//  * @param first first number
+//  * @type {number}
+//  */
+// var first = parseInt( prompt('Enter first number') );
+// /**
+//  * @param second second number
+//  * @type {number}
+//  */
+// var second = parseInt( prompt('Enter second number') );
+// /**
+//  * @param sum sum of two numbers (first and second)
+//  * @type {number}
+//  */
+// var sum = first + second;
+// /**
+//  * @description print to console sum 
+//  */
+// console.log( sum );
+
+// Имитация бросания двух игральных костей
+// setInterval(() => {
+//     console.log( Math.round( Math.random() ) );
+// }, 500);
