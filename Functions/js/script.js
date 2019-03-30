2900e528aa47a66f8b0445da61b0de6a699f2588
Alexander Vasuluev <alexhateful@gmail.com>
2019-03-30 10:24:34 +0200

    add functions example

---
+++ Functions/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * @description Функция возвращяет результат от умножения чисел a и b
+ * @param {number} a 
+ * @param {number} b
+ * @returns {number} a * b
+ * @example 2 * 3 = 6
+ */
+function square( a, b ) {
+    return a * b;
+}
+
+/**
+ * @description запрашиваем у пользователя строку возвращяем число
+ * @param {string} string
+ * @returns {number} парсит введенное число 
+ */
+function askNumber( string ) {
+    let chislo;
+    
+    return parseInt( chislo );
+}
+
+do {
+    
+    let a = askNumber( 'Введите первое число' );
+    
+    let b = askNumber( 'Введите второе число' );
+
+    let S = square( a, b );
+
+    alert( `Площадь прямоугольника со сторонами ${a} и ${b} равна ${S}` );
+
+} while ( confirm( "Хотите повторить?" ) );
