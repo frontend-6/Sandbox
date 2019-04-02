0a54d2a13e5a82689bf6a9b6b32be7947ff23e67
alexhateful <alexhateful@gmail.com>
2019-04-02 20:04:41 +0300

    update

--- Test/js/script.js
+++ Test/js/script.js
@@ -1,20 +1,68 @@
-const content = {
-    "Трава зеленая?": true,
-    "Динозавры путешествуют по земле?": false,
-    "19?": false 
-}
+// const content = {
+//     "Трава зеленая?": true,
+//     "Динозавры путешествуют по земле?": false,
+//     "19?": false 
+// }
 
-let howMuchIsRight = 0;
+// let howMuchIsRight = 0;
 
-for (const key in content) {
-    if (content.hasOwnProperty(key)) {
-        const value = content[key];
-        value == confirm(`${key}`) ? howMuchIsRight++ : '';
-    }
-}
+// for (const key in content) {
+//     if (content.hasOwnProperty(key)) {
+//         const value = content[key];
+//         value == confirm(`${key}`) ? howMuchIsRight++ : '';
+//     }
+// }
 
-alert(`Количество правильных ответов = ${howMuchIsRight}`);
+// alert(`Количество правильных ответов = ${howMuchIsRight}`);
 
 // function ShowAlert() {
 //     alert("Вы нажали на кнопку!");
 // }
+
+var salaries = {
+    "Вася": 100,
+    "Петя": 300,
+    "Даша": 250
+};
+
+let sum = 0;
+
+for (const key in salaries) {
+    if (salaries.hasOwnProperty(key)) {
+        const value = salaries[key];
+        sum += value;
+    }
+}
+
+console.log(sum);
+
+// var salaries = [100, 300, 250];
+
+// salaries[0];
+// salaries[2];
+
+// let sum = 0;
+
+// for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//         const value = salaries[key];
+//         sum += value;
+//     }
+// }
+
+// console.log(sum);
+
+// var salaries = [100, 300, 250];
+// var salaries = {
+//     "Вася": 100,
+//     "Петя": 300,
+//     "Даша": 250
+// };
+
+// let sum = 0;
+
+// for (let i = 0; i < salaries.length; i++) {
+//     const element = salaries[i];
+//     sum += element;
+// }
+// console.log(sum);
