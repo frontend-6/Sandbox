6bf401cc930e0a9bf8d5421bc085abed9233d337
alexhateful <alexhateful@gmail.com>
2019-04-04 20:23:38 +0300

    PLEASE FIX THIS!

--- Resheto Eratosfena/js/script.js
+++ Resheto Eratosfena/js/script.js
@@ -1,90 +1,109 @@
-let min = 3 , max = 10;
+// let min = 3 , max = 10;
+ 
+// let is_chislo_prostoe = true;// будем проверять является ли число простым
 
-let is_chislo_prostoe = true;// будем проверять является ли число простым
-
-/**
- * 
- * @param {number} min 
- * @param {number} max 
- */
-function Resheto(min, max) {
+// /**
+//  * 
+//  * @param {number} min 
+//  * @param {number} max 
+//  */
+// Resheto(min, max);
+// function Resheto(min, max) {
     
-    for (let i = min; i <= max; i++) {
+//     for (let i = min; i <= max; i++) {
         
-        for (let j = 2; j < i; j++) {
+//         for (let j = 2; j < i; j++) {
             
-            if ( i % j == 0) {
+//             if ( i % j == 0 ) {
 
-                // is_chislo_prostoe = false;
-                break;
+//                 // is_chislo_prostoe = false;
+//                 break;
 
-            }
+//                 console.log(123);
 
-            if ( j == i - 1 ) {
-                console.log(i);
-            }
+//             }
+
+//             if ( j == i - 1 ) {
+//                 console.log(i);
+//             }
             
-        }
+//         }
 
-        // if (is_chislo_prostoe) console.log(i);
-        // is_chislo_prostoe = true;
-    }
+//         // if (is_chislo_prostoe) console.log(i);
+//         // is_chislo_prostoe = true;
+//     }
 
+// }
+
+// // 7 = 2*3+1; is_prostoe
+// // 6 = 2*3; ne_prostoe
+// // 12 = 3*4/6*2/2*2*3; ne_prostoe
+
+// console.log(`Вариант вывода 1`);
+// var salaries = {
+//     "Вася": 100,
+//     "Петя": 300,
+//     "Даша": 250
+// };
+// for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//         const value = salaries[key];
+//         console.log(`key = ${key}, value = ${value}`);
+//     }
+// }
+
+// console.log(`Вариант вывода 2`);
+// var salaries = {
+//     "0": 100,
+//     "1": 300,
+//     "5": 250
+// };
+// for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//         const value = salaries[key];
+//         console.log(`key = ${key}, value = ${value}`);
+//     }
+// }
+
+// console.log(`Вариант вывода 3`);
+// var salaries = [ 100, 300, 250 ];
+// for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//         const value = salaries[key];
+//         console.log(`key = ${key}, value = ${value}`);
+//     }
+// }
+
+// console.log(`Вариант вывода 4`);
+// var salaries = {
+//     0: 100,
+//     1: 300,
+//     5: 250
+// };
+// for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//         const value = salaries[key];
+//         console.log(`key = ${key}, value = ${value}`);
+//     }
+// }
+
+// window.myName = "Sasha";
+
+// console.log(window.myName);
+
+let cols = 4, rows = 4;
+
+let counter = 1;
+
+var arr = [];
+
+for (let a = 0; a < cols; a++) {
+    arr[a] = [];
 }
 
-Resheto(min, max);
-// 7 = 2*3+1; is_prostoe
-// 6 = 2*3; ne_prostoe
-// 12 = 3*4/6*2/2*2*3; ne_prostoe
-
-console.log(`Вариант вывода 1`);
-var salaries = {
-    "Вася": 100,
-    "Петя": 300,
-    "Даша": 250
-};
-for (const key in salaries) {
-    if (salaries.hasOwnProperty(key)) {
-        const value = salaries[key];
-        console.log(`key = ${key}, value = ${value}`);
-    }
+for (let i = 0, j = 0, k = cols, l = rows; i < k, j < l, k > i, l > j; i++, j++, k--, l--) {
+    
+    arr[i][j] = counter++;
 }
 
-console.log(`Вариант вывода 2`);
-var salaries = {
-    "0": 100,
-    "1": 300,
-    "5": 250
-};
-for (const key in salaries) {
-    if (salaries.hasOwnProperty(key)) {
-        const value = salaries[key];
-        console.log(`key = ${key}, value = ${value}`);
-    }
-}
-
-console.log(`Вариант вывода 3`);
-var salaries = [ 100, 300, 250 ];
-for (const key in salaries) {
-    if (salaries.hasOwnProperty(key)) {
-        const value = salaries[key];
-        console.log(`key = ${key}, value = ${value}`);
-    }
-}
-
-console.log(`Вариант вывода 4`);
-var salaries = {
-    0: 100,
-    1: 300,
-    5: 250
-};
-for (const key in salaries) {
-    if (salaries.hasOwnProperty(key)) {
-        const value = salaries[key];
-        console.log(`key = ${key}, value = ${value}`);
-    }
-}
-
-window.myName = "Sasha";
-
-console.log(window.myName);
+console.log(arr);
