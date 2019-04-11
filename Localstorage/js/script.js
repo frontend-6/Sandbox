7655a7b1e48f154245d12f0abae35cb9d716f5fc
alexhateful <alexhateful@gmail.com>
2019-04-11 20:07:58 +0300

    add localstorage example

---
+++ Localstorage/js/script.js
@@ -0,0 +1,25 @@
+let data = localStorage.getItem("name") ? localStorage.getItem("name") : '';
+
+let name = data ? JSON.parse(data) : new Array();
+
+function Esche() {
+    name.push(prompt("Имя?"));
+
+    Print(name);
+
+    let temp = JSON.stringify(name);  
+    
+    localStorage.setItem("name", temp);
+}
+
+function Print(_message) {
+    document.getElementById("app").innerHTML = _message;
+}
+
+document.getElementById("js-button").addEventListener("click", Esche);
+
+document.getElementById("js-clear").addEventListener("click", () => {
+    localStorage.clear();
+
+    Print("");
+})
